1b44404c64c9be2ed25393aabf16688592dcef23
Mohd Ali <mohd.ali@bertelsmann.de>
2018-08-13 22:26:46 +0800

    Add project files.

---
+++ AskKirk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AskKirk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
